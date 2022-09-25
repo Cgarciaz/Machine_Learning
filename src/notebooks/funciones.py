b57e1dc7b8b59e54c2e9fb320626995a6aa97927
Cgarciaz <cgarciazaravia@gmail.com>
2022-09-25 13:55:09 +0200

    Update funciones.py

--- src/notebooks/funciones.py
+++ src/notebooks/funciones.py
@@ -19,3 +19,11 @@ def get_data(col_index_name , col_target_name, dataframe): #col_name es la colum
     cont += 1
     
   return pd.Series(datos)
+
+# ML
+n_folds = 5
+
+def rmsle_cv(model):
+    kf = KFold(n_folds, shuffle=True, random_state=42).get_n_splits(X_train.values)
+    rmse= np.sqrt(-cross_val_score(model, X_train.values, y_train, scoring="neg_mean_squared_error", cv = kf))
+    return(rmse)
