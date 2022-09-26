907be49b2bcc339fa620727a9f6c4115d7054af9
Cgarciaz <cgarciazaravia@gmail.com>
2022-09-26 22:54:02 +0200

    Update app_model.py

--- src/app_model.py
+++ src/app_model.py
@@ -16,6 +16,8 @@ def predecir():
     json = request.get_json(force=True)
     medidas = json['Medidas']
     clasificador = joblib.load('my_model.pkl')
+    prediccion = clasificador.predict(medidas)
+    return 'las medidas que diste corresponden a la clase {0}'.format(prediccion )
 
 if __name__ == '__main__':
     app.run()
