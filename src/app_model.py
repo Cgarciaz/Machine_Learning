36c6b513ac90c1f5382f05715990ef89f5cfd358
Cgarciaz <cgarciazaravia@gmail.com>
2022-09-27 00:46:43 +0200

    Update app_model.py

--- src/app_model.py
+++ src/app_model.py
@@ -1,20 +1,60 @@
-# from crypt import methods
-from flask import Flask, jsonify, request
+from flask import Flask, session, url_for, render_template, redirect
 import joblib
 
+from season import Season
+
+# Cargamos los modelos guardados en saved_models
+knn_loaded = joblib.load("saved_models/my_model.pkl")
+# encoder_loaded = joblib.load("saved_models/iris_label_encoder.pkl")
+
+# Creamos la función de predicción
+def make_prediction(model, encoder, sample_json):
+    SepalLengthCm = sample_json['SepalLengthCm']
+    SepalWidthCm = sample_json['SepalWidthCm']
+    PetalLengthCm = sample_json['PetalLengthCm']
+    PetalWidthCm = sample_json['PetalWidthCm']
+    
+    # Creamos un vector de input
+    flower = [[SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm]]
+
+    # Predicción
+    prediction_raw = model.predict(flower)
+
+    # Convertimos los índices en labels de las clases
+    prediction_real = encoder.inverse_transform(prediction_raw)
+
+    return prediction_real[0]
+
+# creamos la app de Flask
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
 
-@app.route("/")
-def home():
-    return "la pagina esta funcionando"
+@app.route('/', methods=['GET','POST'])
+def index():
+    form = FlowerForm()
 
-@app.route("/predecir",  methods = ["POST"])
-def predecir():
-    json = request.get_json(force=True)
-    medidas = json['Medidas']
-    clasificador = joblib.load('my_model.pkl')
-    prediccion = clasificador.predict(medidas)
-    return 'las medidas que diste corresponden a la clase {0}'.format(prediccion )
+    if form.validate_on_submit():
+        session['SepalLengthCm'] = form.SepalLengthCm.data
+        session['SepalWidthCm'] = form.SepalWidthCm.data
+        session['PetalLengthCm'] = form.PetalLengthCm.data
+        session['PetalWidthCm'] = form.PetalWidthCm.data
 
+        return redirect(url_for('prediction'))
+    return render_template("home.html", form=form)
+
+@app.route('/prediction', methods=['POST','GET'])
+def prediction():
+    content = {'SepalLengthCm': float(session['SepalLengthCm']), 'SepalWidthCm': float(session['SepalWidthCm']),
+               'PetalLengthCm': float(session['PetalLengthCm']), 'PetalWidthCm': float(session['PetalWidthCm'])}
+
+    results = make_prediction(knn_loaded, encoder_loaded, content)
+
+    return render_template('prediction.html', results=results)
+
+# Ejecutamos la aplicación app.run()
 if __name__ == '__main__':
+    # LOCAL
+    # app.run(host='0.0.0.0', port=8080)
+
+    # REMOTO
     app.run()
