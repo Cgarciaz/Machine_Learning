158c77705bc5f174a6de969f171089f37812aa5a
Cgarciaz <cgarciazaravia@gmail.com>
2022-09-27 00:56:27 +0200

    Update season.py

--- src/season.py
+++ src/season.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
-class Season(FlaskForm):
+class AnimeFeature(FlaskForm):
     Start_season = StringField('Season (0-3)')
     Type = StringField('Type (0-5)')
     Episodes = StringField('Episodes (max 350)')
