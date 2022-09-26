e21ed2bdfe047d139e28e1382d20645e0f112898
Cgarciaz <cgarciazaravia@gmail.com>
2022-09-26 12:09:43 +0200

    Update train.py

--- src/train.py
+++ src/train.py
@@ -23,16 +23,24 @@ from utils.cleaning import Cleaning
 
 # Load data into database
 # df = pd.read_csv(path + "src\data\raw\data.csv")
-df_temp = pd.read_csv("data/raw/data.csv", na_values = ['Unknown'], on_bad_lines='skip')
+df_temp = pd.read_csv("data/raw/data.csv", sep=',', na_values = ['Unknown'])
 df_season = pd.read_csv("data/raw/anime_season.csv")
 
 print(f"Número total de registros ant: {len(df_temp)}")
 print(f"Número total de column ant: {len(df_temp.columns)}")
+print(f"Número total de column: {df_season.head()}")
 
 # Process data
-df_temp = Cleaning.drop(df_temp)
+df_temp = Cleaning.transform(df_season,df_temp)
 
 print(f"Número total de registros: {len(df_temp)}")
 print(f"Número total de column: {len(df_temp.columns)}")
+print(f"Número total de column: {df_temp.head()}")
+
+# # Process data
+# df_temp = Cleaning.drop(df_temp)
+
+# print(f"Número total de registros: {len(df_temp)}")
+# print(f"Número total de column: {len(df_temp.columns)}")
 
 exit()
