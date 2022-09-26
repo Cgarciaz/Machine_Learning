3e3c33e363593d6fb4207475405d69265b1a7419
Cgarciaz <cgarciazaravia@gmail.com>
2022-09-26 12:09:40 +0200

    Update cleaning.py

--- src/utils/cleaning.py
+++ src/utils/cleaning.py
@@ -1,7 +1,8 @@
-
+import pandas as pd
+import numpy as np
 
 class Cleaning():
-  
+
   def drop(df):
     
     df.drop(['Name' , 'English name' , 'Japanese name','Watching', 'On-Hold', 'Dropped', 'Ranked', 'Popularity',
@@ -13,7 +14,7 @@ class Cleaning():
 
     return df
 
-  def transform(df):
+  def transform(df_season, df):
     
     # Creación de campo para obtener el año de emision del anime
     df['Start_year'] = df['Aired'].dropna(axis = 0).apply(lambda x : x.split('to')[0]).apply(
@@ -34,6 +35,18 @@ class Cleaning():
                                         + x['Score-7']*7 + x['Score-6']*6 + x['Score-5']*5 + x['Score-4']*4 + x['Score-3']*3 + x['Score-2']*2
                                         + x['Score-1'])/(x['Score-10'] + x['Score-9'] + x['Score-8']+ x['Score-7'] + x['Score-6'] + x['Score-5']
                                         + x['Score-4'] + x['Score-3'] + x['Score-2']+ x['Score-1'])))
+
+    df1 = pd.merge(
+    left=df,right=df_season, how='left', left_on='Start_Month', right_on='Month_emision'
+    )
+
+    df1['Start_season'] = np.where(df1['Season_x'].isna(),
+                                        df1['Season_y'],df1['Season_x'])
+
+    df1['New_score2'] = np.where(df1['Score'].isna(),
+                                    df1['New_score'],df1['Score'])
+  
+    # df1.rename(columns={'New_score2':'Score'}, inplace=True)
 
     print("transform process was successful")
 
