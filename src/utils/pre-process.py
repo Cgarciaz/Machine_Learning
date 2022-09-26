3db25e87de5745056cab943360a138717d73a2fc
Cgarciaz <cgarciazaravia@gmail.com>
2022-09-26 17:21:38 +0200

    Create pre-process.py

---
+++ src/utils/pre-process.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+class PreProcess():
+
+  def transform(df_season, df):
+    
+    print("drop process was successful")
+
+    return df
+
+  def transform2(df):
+
+    print("transform2 process was successful")
+
+    return df
