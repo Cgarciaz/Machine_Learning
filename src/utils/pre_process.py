2fedb793cab79ff194437d0e9b459e9051e75817
Cgarciaz <cgarciazaravia@gmail.com>
2022-09-28 13:09:50 +0200

    Update pre_process.py

--- src/utils/pre_process.py
+++ src/utils/pre_process.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 class PreProcess():
@@ -6,16 +5,16 @@ class PreProcess():
   def transform(df):
 
     df = df[['Start_season','Type','Episodes','Rating','Score']]
+
     # Para convertir las categóricas en binarias debemos pasarla primero a booleanas
     # evaluando si son del tipo object o category
     categorical_feature = (df.dtypes == "category") | (df.dtypes == object)
     categorical_cols = df.columns[categorical_feature].tolist()
 
     for c in categorical_cols:
-      lbl = LabelEncoder() 
-      lbl.fit(list(df[c].values)) 
-      df[c] = lbl.transform(list(df[c].values))
-
+        lbl = LabelEncoder() 
+        lbl.fit(list(df[c].values)) 
+        df[c] = lbl.transform(list(df[c].values))
 
     print("transform process was successful")
 
