8022f7efe54361bf190e93a52ad31c7c411f75f4
Cgarciaz <cgarciazaravia@gmail.com>
2022-09-28 12:33:20 +0200

    Update test_ml.py

--- src/test_ml.py
+++ src/test_ml.py
@@ -1,17 +1,18 @@
 # Fichero test.py
+import joblib
 
 # Realizar unas predicciones de prueba
-classifier_loaded = joblib.load("saved_models/knn_iris_dataset.pkl")
-encoder_loaded = joblib.load("saved_models/iris_label_encoder.pkl")
+classifier_loaded = joblib.load("model/my_model.pkl")
+encoder_loaded = joblib.load("model/anime_label_encoder.pkl")
 print(" --- Pickle classifier y label encoder load executed ---")
 
 # Prediction test con vectores aleatorios
-X_manual_test = [[20.1, 20.1, 100.1, 100.1]]
+X_manual_test = [[3,5,12,3]] # real: 6.96 - predict: 7.07
 print("X_manual_test", X_manual_test)
 
 prediction_raw = classifier_loaded.predict(X_manual_test)
 print("Prediction_raw", prediction_raw)
 
 prediction_real = encoder_loaded.inverse_transform(
-                            classifier.predict(X_manual_test))
+                            classifier_loaded.predict(X_manual_test))
 print("Real prediction", prediction_real)
