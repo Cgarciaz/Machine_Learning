5cbbf071c1c7b9fe62aec6b3c9998cff5cc5856e
Cgarciaz <cgarciazaravia@gmail.com>
2022-09-29 16:49:57 +0200

    Update feature.py

--- src/feature.py
+++ src/feature.py
@@ -2,9 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 class AnimeFeature(FlaskForm):
-    Start_season = StringField('Season (0-3)')
-    Type = StringField('Type (0-5)')
+    Start_season = StringField('Season')
+    Type = StringField('Type')
     Episodes = StringField('Episodes (max 350)')
-    Rating = StringField('Rating (0-5)')
+    Rating = StringField('Rating')
 
     submit = SubmitField("Predict")
