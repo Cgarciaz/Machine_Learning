b52459133276b727703148bfca4cdbb90e8efb2f
Cgarciaz <cgarciazaravia@gmail.com>
2022-10-01 06:36:24 +0200

    Update pre_process.py

--- src/utils/pre_process.py
+++ src/utils/pre_process.py
@@ -4,8 +4,6 @@ class PreProcess():
 
   def transform(df):
 
-    df = df[['Start_season','Type','Episodes','Rating','Score']]
-
     # Para convertir las categóricas en binarias debemos pasarla primero a booleanas
     # evaluando si son del tipo object o category
     categorical_feature = (df.dtypes == "category") | (df.dtypes == object)
