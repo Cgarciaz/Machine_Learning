b728d240424339e9075c2569ccdc919e2d6df5d4
Cgarciaz <cgarciazaravia@gmail.com>
2022-10-01 06:36:58 +0200

    Update feature.py

--- src/feature.py
+++ src/feature.py
@@ -4,7 +4,7 @@ from wtforms import StringField, SubmitField
 class AnimeFeature(FlaskForm):
     Start_season = StringField('In What Season Will It Start?')
     Type = StringField('What Type Of Distribution?')
-    Episodes = StringField('How Many Episodes? (max 350)')
+    Episodes = StringField('How Many Episodes?')
     Rating = StringField('What Rating?')
 
     submit = SubmitField("Predict")
